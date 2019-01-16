df2016ad65040924df3141c84fab8ff8253a3d6c
Stanton Parham <stanton8parham8@gmail.com>
2019-01-15 21:55:45 -0500

    Implemented remaining fuel calculations

--- FuelPoller.py
+++ FuelPoller.py
@@ -3,43 +3,99 @@
 from PyQt5.QtCore import pyqtSignal
 from SensorPoller import SensorPoller
 
-isRPI = True
-if (isRPI):
+# import os to check if the machine is a RPI
+import os
+machineIsRaspberryPi = os.uname()[4].startswith("arm")
+
+# if running on a RPI
+if (machineIsRaspberryPi):
+    # import libraries related to reading from the GPIO of the RPI
     import busio
     import digitalio
     import board
     import adafruit_mcp3xxx.mcp3008 as MCP
     from adafruit_mcp3xxx.analog_in import AnalogIn
 
+    # this is the pin on the MCP3008 that is connected to the hall sensor
+    # on the fuel tank
+    FUEL_HALL_SENSOR_MCP_PIN = MCP.P0
+
+# the range of voltages that do not warrant any significant magnetic field
+# detection by the hall sensor on the fuel tank
+NO_MAG_FIELD_VOLTAGE_RANGE = [0.97, 1.03]
+
+# the lowest voltage value given by the hall sensor
+LOWEST_VOLTAGE = 0.05
+
+# the highest voltage value given by the hall sensor
+HIGHEST_VOLTAGE = 2.01
+
+# the difference between the lowest voltage value and the low end of the
+# no-mag-field voltage range
+DIF_LOWEST_VOLTAGE_NMF_RANGE = NO_MAG_FIELD_VOLTAGE_RANGE[0] - LOWEST_VOLTAGE
+
+# the difference between the highest voltage value and the high end of the
+# no-mag-field voltage range
+DIF_HIGHEST_VOLTAGE_NMF_RANGE = HIGHEST_VOLTAGE - NO_MAG_FIELD_VOLTAGE_RANGE[1]
+
+# the percentage (as a value between 0.0 and 1.0) of fuel remaining when the
+# hall sensor first detects a magnetic field
+INITIAL_PERC_FUEL = 0.5 # the hall sensor can detect the magnet when there is
+                        # approximately half a tank of fuel remaining
+
 class FuelPoller(SensorPoller):
+    """
+    This object serves as an independent thread that polls for a fuel level.
+    """
+    # signal that emits the current fuel level as a float (between 0.0 and 1.0)
     signal = pyqtSignal(float)
-    fuel = 1.0
+    # the percentage (as a value between 0.0 and 1.0) of fuel remaining
+    remainingFuel = 0.6
 
     def __init__(self, pollingRate):
         super().__init__(pollingRate)
-        if (isRPI):
+        # if running on a RPI
+        if (machineIsRaspberryPi):
             # create the spi bus
             spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-
             # create the cs (chip select)
             cs = digitalio.DigitalInOut(board.D5)
-
             # create the mcp object
             mcp = MCP.MCP3008(spi, cs)
-
             # create an analog input channel on pin 0
-            self.chan0 = AnalogIn(mcp, MCP.P0)
+            self.sensorChannel = AnalogIn(mcp, FUEL_HALL_SENSOR_MCP_PIN)
 
     def poll(self):
-        # TODO read from hall sensor on fuel tank
-        # TODO calculate fuel level based on magnetic intensity
-        self.fuel = self.fuel - 0.05
-        if (self.fuel < 0):
-            self.fuel = 1.0
+        # calculate the remaining fuel
+        self.calculateRemainingFuel()
+        # emit fuel percentage value
+        self.signal.emit(self.remainingFuel)
 
-        if (isRPI):
-            print('Raw ADC Value: ', self.chan0.value)
-            print('ADC Voltage: ' + str(self.chan0.voltage) + 'V')
+    def calculateRemainingFuel(self):
+        # if running on a RPI
+        if (machineIsRaspberryPi):
+            voltage = self.sensorChannel.voltage
+            # print('Raw ADC Value: ', self.sensorChannel.value)
+            # print('ADC Voltage: ' + str(self.sensorChannel.voltage) + 'V')
 
-        # emit with value
-        self.signal.emit(self.fuel)
+            # calculate fuel level based on magnetic intensity
+            # if the voltage is within the no-mag-field range
+            if (voltage >= NO_MAG_FIELD_VOLTAGE_RANGE[0] and voltage <= NO_MAG_FIELD_VOLTAGE_RANGE[1]):
+                # the fuel level is guaranteed to be at least at the initial
+                # level at which the magnet can be detected
+                self.remainingFuel = INITIAL_PERC_FUEL
+            # else if the voltage is below the no-mag-field range
+            else if (voltage < NO_MAG_FIELD_VOLTAGE_RANGE[0]):
+                fuelUsed = (NO_MAG_FIELD_VOLTAGE_RANGE[0] - voltage) / (DIF_LOWEST_VOLTAGE_NMF_RANGE) * INITIAL_PERC_FUEL
+                self.remainingFuel = INITIAL_PERC_FUEL - fuelUsed
+            # else if the voltage is above the no-mag-field range
+            else if (voltage > NO_MAG_FIELD_VOLTAGE_RANGE[1]):
+                fuelUsed = (voltage - NO_MAG_FIELD_VOLTAGE_RANGE[1]) / (DIF_HIGHEST_VOLTAGE_NMF_RANGE) * INITIAL_PERC_FUEL
+                self.remainingFuel = INITIAL_PERC_FUEL - fuelUsed
+        else:
+            # just do a quick calculation to change the remaining fuel in a meaningful way
+            self.remainingFuel = self.remainingFuel - 0.063
+            if (self.remainingFuel < 0):
+                self.remainingFuel = 1.0
+
+        print('Remaining fuel: ' + self.remainingFuel)
