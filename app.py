43ee86d51a2d01e8b45b93f96755f22e9ea03ba1
Stanton Parham <stanton8parham8@gmail.com>
2019-07-21 23:19:36 -0400

    Modify app.py to serve static files from client/build/

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 #! /usr/bin/env python
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+app = Flask(__name__, static_url_path='')
+
+STATIC_DIR = 'client/build'
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return send_from_directory(STATIC_DIR, 'index.html')
+
+@app.route('/<path>')
+def send_js(path):
+    return send_from_directory(STATIC_DIR, path)
+
+@app.route('/<path:base>/<path:path>')
+def send_files(base, path):
+    return send_from_directory(STATIC_DIR, base + '/' + path)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
