2d308099d893478598c7896fbd712b9d7fe6ccb8
Stanton Parham <stanton8parham8@gmail.com>
2019-09-07 17:15:58 -0400

    Add hardware folder and skeleton for HardwareManager

---
+++ server/hardware/HardwareManager.py
@@ -0,0 +1,10 @@
+class HardwareManager():
+  def __init__(self, callback):
+    super().__init__()
+    self.cb = callback
+
+  def startPollers(self):
+    pass
+
+  def stopPollers(self):
+    pass
