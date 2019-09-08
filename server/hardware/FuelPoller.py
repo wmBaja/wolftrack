1530bb589e9fa35d743693f25c2c1b5213b35144
Stanton Parham <stanton8parham8@gmail.com>
2019-09-07 23:04:32 -0400

    Implement FuelPoller to interface with GPIO libs and mock values

--- server/hardware/FuelPoller.py
+++ server/hardware/FuelPoller.py
@@ -1,21 +1,133 @@
 from .SensorPoller import SensorPoller
 
-import random
+import math
+
+# import os to check if the machine is a RPI
+import os
+machineIsRaspberryPi = os.uname()[4].startswith('arm')
+
+# if running on a RPI
+if (machineIsRaspberryPi):
+  # import libraries related to reading from the GPIO of the RPI
+  import busio
+  import digitalio
+  import board
+  import adafruit_mcp3xxx.mcp3008 as MCP
+  from adafruit_mcp3xxx.analog_in import AnalogIn
+
+  # this is the pin on the MCP3008 that is connected to the Hall sensor
+  # on the fuel tank
+  FUEL_HALL_SENSOR_MCP_PIN = MCP.P0
+
+# the lowest voltage value given by the hall sensor
+LOWEST_VOLTAGE = 0.05
+
+# the middle voltage given by the hall sensor
+# This is the voltage when no magnetic field is being experienced by the
+# hall sensor.
+MIDDLE_VOLTAGE = 1.00
+
+# the highest voltage value given by the hall sensor
+HIGHEST_VOLTAGE = 2.01
+
+# the maximum number of liters of fuel that the tank can hold
+MAX_FUEL_CAPACITY = 3.45
+# the maximum number of liters of fuel that the hall sensor can detect
+MAX_DETECTED_FUEL_CAPACITY = 1.6
+
+# the weight of each successive fuel calculation
+EMA_WEIGHT = 0.1
+
+# the file name for a file of mocked voltages for the fuel
+MOCKED_FUEL_DATA_FILE_NAME = './hardware/mock_voltages.txt'
 
 class FuelPoller(SensorPoller):
   """
   An independent thread that polls for a fuel level.
   """
+
+  # the percentage (as a value between 0.0 and 1.0) of fuel remaining
+  remainingPerc = 0
+  # the raw liters of remaining fuel
+  remainingLiters = MAX_DETECTED_FUEL_CAPACITY
+  # the liters of remaining fuel based on an exponential moving average
+  emaLiters = MAX_DETECTED_FUEL_CAPACITY
+
   def __init__(self, pollingRate, callback):
     super().__init__(pollingRate, callback)
+    # if running on a RPI
+    if (machineIsRaspberryPi):
+      # create the spi bus
+      spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+      # create the cs (chip select)
+      cs = digitalio.DigitalInOut(board.D5)
+      # create the mcp object
+      mcp = MCP.MCP3008(spi, cs)
+      # create an analog input channel on pin 0
+      self.sensorChannel = AnalogIn(mcp, FUEL_HALL_SENSOR_MCP_PIN)
+    else:
+      # read in mock values from a file
+      with open(MOCKED_FUEL_DATA_FILE_NAME) as file:
+        lines = file.readlines()
+        self.mocked_data = [float(val) for val in lines]
+        self.mocked_data_idx = 0
 
   def poll(self):
     """
     Calculates the current fuel level based on the voltage read from the
     Hall-effect sensor.
     """
-    return random.random()
-    # pass
+    # calculate the remaining fuel
+    self.calculateRemainingFuel()
+    return {
+      'remainingPercentage': self.remainingPerc,
+      'remainingLiters': self.remainingLiters,
+      'remainingLitersEMA': self.emaLiters
+    }
+
+  def calculateRemainingFuel(self):
+    """
+    Calculates the remaining fuel in the tank based on a voltage value.
+    """
+    voltage = MIDDLE_VOLTAGE
+    # if running on a RPI
+    if (machineIsRaspberryPi):
+      voltage = self.sensorChannel.voltage
+      # print('Raw ADC Value: ', self.sensorChannel.value)
+    else:
+      # just get the next value from the mock data
+      voltage = self.mocked_data[self.mocked_data_idx]
+      # update the index
+      self.mocked_data_idx += 1
+      if (self.mocked_data_idx >= len(self.mocked_data) + 1):
+        self.mocked_data_idx = 0
+    print('ADC Voltage: ' + str(voltage) + 'V')
+
+    distFromCenterVoltage = abs(voltage - 1)
+
+    self.remainingLiters = self.voltsToLiters(distFromCenterVoltage)
+    self.emaLiters = self.calcEMA(self.emaLiters, self.remainingLiters)
+    self.remainingPerc = self.emaLiters / MAX_FUEL_CAPACITY
+
+    print('Remaining percentage: ' + '{:.0f}'.format(self.remainingPerc * 100) + '%')
+    print('Remaining liters: ' +  '{:.2f}'.format(self.remainingLiters))
+    print('EMA liters: ' +  '{:.2f}'.format(self.emaLiters))
+
+  @staticmethod
+  def voltsToLiters(voltage):
+    """
+    Returns the number of liters of fuel remaining based on the given
+    normalized voltage.
+    This uses an equation that was derived from manual tests.
+    """
+    return -0.176 + -0.648 * math.log(voltage)
+
+  @staticmethod
+  def calcEMA(curAvg, newVal):
+    """
+    Returns the exponential moving average given the current average and a new value.
+    """
+    return EMA_WEIGHT * newVal + (1 - EMA_WEIGHT) * curAvg
 
   def cleanup(self):
     """
