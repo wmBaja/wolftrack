83e541544c65933e54a788dce5bcccc5ddf021ff
Stanton Parham <stanton8parham8@gmail.com>
2019-09-07 23:53:51 -0400

    Add GPSPoller to poller pool

--- server/hardware/HardwareManager.py
+++ server/hardware/HardwareManager.py
@@ -1,16 +1,21 @@
 from functools import partial
 
 from .FuelPoller import FuelPoller
+from .GPSPoller import GPSPoller
 
 FUEL_DATA = 'fuel_data'
 FUEL_POLLING_RATE = 1
+
+GPS_DATA = 'gps_data'
+GPS_POLLING_RATE = 0.5
 
 class HardwareManager():
   def __init__(self, callback):
     super().__init__()
     self.onData = callback
     self.pollerPool = [
-      FuelPoller(FUEL_POLLING_RATE, partial(self.onData, FUEL_DATA))
+      FuelPoller(FUEL_POLLING_RATE, partial(self.onData, FUEL_DATA)),
+      GPSPoller(GPS_POLLING_RATE, partial(self.onData, GPS_DATA))
     ]
 
   def startPollers(self):
