5115c02f3b3866e99f236093e06bec9af7facceb
Stanton Parham <stanton8parham8@gmail.com>
2019-09-14 17:04:09 -0400

    Add latest client build

---
+++ client/build/precache-manifest.845ffe02faf8b69258a5c09a95c12583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ead65caf622f068ce109ac383976b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f806c101c0174caebc54",
+    "url": "/static/css/main.2a748fdd.chunk.css"
+  },
+  {
+    "revision": "d4edc50a50d1b2fb39dc",
+    "url": "/static/js/2.ce1a2ade.chunk.js"
+  },
+  {
+    "revision": "f806c101c0174caebc54",
+    "url": "/static/js/main.cd2fe6a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d6ca991a7e2746a7e3e1640d04fa2a7",
+    "url": "/static/media/test.0d6ca991.geojson"
+  }
+]);
