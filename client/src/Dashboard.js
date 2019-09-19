966157bf019266221bc188a51e2f7b499a4be57b
Stanton Parham <stanton8parham8@gmail.com>
2019-09-19 19:44:43 -0400

    Update to display speed; update to display fuel perc better

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
 import './Dashboard.css';
-import { sendMsg } from './api.js';
 
 function Dashboard({ data }) {
-  const { fuelData } = data;
+  const { fuelData, gpsData } = data;
 
   const formattedPercentage = (fuelData.remainingPercentage * 100).toPrecision(2);
-  const formattedSpeed = (34.56783).toPrecision(2);
+  const formattedSpeed = (gpsData.speed).toPrecision(2);
 
   return (
     <div className="Dashboard">
-      {/* <div> */}
-        <button onClick={sendMsg} className="Message">Send a message</button>
-      {/* </div> */}
       <div className="Fuel">
-        {formattedPercentage}
+        <div>
+          <div class="Dashboard-data-num">{formattedPercentage}</div>
+          <div class="Dashboard-data-unit">% Fuel</div>
+        </div>
       </div>
       <div className="Speed">
-        {formattedSpeed}
+      <div>
+          <div class="Dashboard-data-num">{formattedSpeed}</div>
+          <div class="Dashboard-data-unit">MPH</div>
+        </div>
       </div>
     </div>
   );
