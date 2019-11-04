fb5e65435a6f8d4e91b5dfe442127e4ad41fc80b
Stanton Parham <stanton8parham8@gmail.com>
2019-11-03 19:10:24 -0500

    Create Observer and Subject classes for Observer pattern

---
+++ rpi/observerPattern/Observer.py
@@ -0,0 +1,11 @@
+class Observer():
+  """
+  An Observer in the Observer design pattern. It receives updates once
+  attached to a Subject.
+  """
+
+  def update(self, updates):
+    """
+    Abstract method; This should be defined by the child class.
+    """
+    pass
