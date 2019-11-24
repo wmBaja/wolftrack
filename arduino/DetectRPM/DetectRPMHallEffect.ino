37d5f8443e0f5d0be08b2ade9a028111a58ed162
Patrick Ivey <iveypb@gmail.com>
2019-11-23 20:31:58 -0500

    Update DetectRPMHallEffect.ino
    Modified the induction RPM code for the hall effect sensor.

--- arduino/DetectRPM/DetectRPMHallEffect.ino
+++ arduino/DetectRPM/DetectRPMHallEffect.ino
@@ -1,4 +1,3 @@
-// In progress of modifying the code for the hall effect sensor.
 // the number of milliseconds in a second
 int MILLIS_IN_SEC = 1000;
 // the number of seconds in a minute
@@ -6,12 +5,12 @@ int SECS_IN_MIN = 60;
 
 // the amount of time between updates (in ms)
 int UPDATE_PERIOD = 1000;
-// the conversion factor for sparks per update period to sparks per minute
-int SPARKS_PER_UPDATE_TO_SPARKS_PER_MIN = SECS_IN_MIN / (UPDATE_PERIOD / MILLIS_IN_SEC);
+// the conversion factor for spikes per update period to spikes per minute
+int SPIKES_PER_UPDATE_TO_SPIKES_PER_MIN = SECS_IN_MIN / (UPDATE_PERIOD / MILLIS_IN_SEC);
 
 // voltage spike threshold (in terms of a range of 0-5V mapped into a 0-1023 range)
-int THRESHOLD = 900; // (1 volt)
-
+int LOWTHRESHOLD = 100; // (min 7 max 210)
+int HIGHTHRESHOLD = 120
 // the maximum amount of time between spikes (in ms)
 int MAX_SPIKE_INTERVAL = 5;
 
@@ -20,10 +19,9 @@ unsigned long curTime = 0;
 // the next time to print an update (in ms)
 unsigned long nextUpdateTime = 0;
 // the last time that a voltage spike occurred (in ms)
-unsigned long lastSpikeTime = 0;
-// the total number of sparks in this update period
-int numSparks = 0;
-
+//unsigned long lastSpikeTime = 0;
+// the total number of spikes in this update period
+int numSpikes = 0;
 
 
 void setup() {
@@ -35,36 +33,35 @@ void setup() {
 }
 
 
-
 void loop() {
   // get sensor reading
   int sensorValue = analogRead(A0);
-  // determine if there is currently a voltage spike
-  bool isSpiking = sensorValue > THRESHOLD ? true : false;
-//  Serial.println(sensorValue);
+  // Serial.println(sensorValue);
   // get the current time
   curTime = millis();
 
-  // if a voltage spike is detected
-  if (isSpiking) {
-    // if the last spike occurred over MAX_SPIKE_INTERVAL milliseconds ago
-    if (curTime > lastSpikeTime + MAX_SPIKE_INTERVAL) {
-      // then a new spark is starting
-      numSparks++;
+  // 
+  if (sensorValue >= HIGHTHRESHOLD) {
+    while (sensorValue >= LOWTHRESHOLD) { 
     }
-    // this time is the new last spike time
-    lastSpikeTime = curTime;
   }
-
+  //
+  else if (sensorValue < LOWTHRESHOLD) {
+    while (sensorValue < HIGHTHRESHOLD) {
+    }
+    //increment how many spikes have happened 
+    numSpikes++;
+  }
+  
   // update every update period
   if (curTime > nextUpdateTime) {
-    // calculate the RPM from the num sparks in this update period
-    int rpm = numSparks * SPARKS_PER_UPDATE_TO_SPARKS_PER_MIN;
+    // calculate the RPM from the num spikes in this update period
+    int rpm = numSpikes * SPIKES_PER_UPDATE_TO_SPIKES_PER_MIN;
     // report RPM
     Serial.println(rpm);
     // calculate the next update time
     nextUpdateTime = curTime + UPDATE_PERIOD;
-    // reset num of sparks
-    numSparks = 0;
+    // reset num of spikes
+    numSpikes = 0;
   }
 }
