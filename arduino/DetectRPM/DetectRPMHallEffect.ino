19472579c89fb55512423654be406451f9ac4660
Patrick Ivey <iveypb@gmail.com>
2019-11-30 23:04:12 -0500

    Update DetectRPMHallEffect.ino
    Fixed a bug I found where the sensorValue would not change :/ also added some better description of the code.

--- arduino/DetectRPM/DetectRPMHallEffect.ino
+++ arduino/DetectRPM/DetectRPMHallEffect.ino
@@ -22,6 +22,8 @@ unsigned long nextUpdateTime = 0;
 //unsigned long lastSpikeTime = 0;
 // the total number of spikes in this update period
 int numSpikes = 0;
+// To see whether the magnet is currently close or not, initialized to false
+bool isClose= false;
 
 
 void setup() {
@@ -39,18 +41,18 @@ void loop() {
   // Serial.println(sensorValue);
   // get the current time
   curTime = millis();
-
-  // 
-  if (sensorValue >= HIGHTHRESHOLD) {
-    while (sensorValue >= LOWTHRESHOLD) { 
-    }
+  
+  //if the sensor value has dropped below the threshold indicating the magnet is close
+  if ((sensorValue < LOWTHRESHOLD) && !isClose) {
+    // then change the current state of the magnet to close
+    isClose= true;
+    //increment how many spikes have happened, will only be performed once per rotation
+    ++numSpikes;
   }
-  //
-  else if (sensorValue < LOWTHRESHOLD) {
-    while (sensorValue < HIGHTHRESHOLD) {
-    }
-    //increment how many spikes have happened 
-    numSpikes++;
+  // If the sensor value is above the threshold indicating the magnet is no longer close
+  else if ((sensorValue > HIGHTHRESHOLD) && isClose) {
+    // then change the current state of the magnet to not close
+    isClose = false;
   }
   
   // update every update period
