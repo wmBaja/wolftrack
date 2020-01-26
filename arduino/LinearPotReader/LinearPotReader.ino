5d9b92c98d6bb0b3a797608ac527a153fc045a8e
Stanton Parham <stanton8parham8@gmail.com>
2020-01-25 23:24:25 -0500

    Add sketch for reading from lin pot in inches

---
+++ arduino/LinearPotReader/LinearPotReader.ino
@@ -0,0 +1,23 @@
+// the baud rate for the serial connection
+#define BAUD_RATE 115200
+// amount of time between updates (in ms)
+#define UPDATE_INTERVAL 50
+
+unsigned long nextUpdateTime;
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  nextUpdateTime = millis() + UPDATE_INTERVAL;
+}
+
+void loop() {
+  unsigned long curTime = millis();
+  if (curTime > nextUpdateTime) {
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(A0); // range: 0 - 1023
+    double actuationIn = 6 - (sensorValue / 1023.0) * 6; // range: 0 - 6
+    Serial.println(actuationIn);
+
+    nextUpdateTime = curTime + UPDATE_INTERVAL;
+  }
+}
