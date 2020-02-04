dae56bb6b06fada5d233a5224539ca0a0e44bacd
Stanton Parham <stanton8parham8@gmail.com>
2020-02-04 16:40:27 -0500

    Add signIn and signOut methods to FirebaseClient

--- pwa/src/Firebase/FirebaseClient.js
+++ pwa/src/Firebase/FirebaseClient.js
@@ -1,6 +1,31 @@
+import * as firebase from 'firebase/app';
+
+import 'firebase/auth';
+
 export default class FirebaseClient {
   constructor() {
-
+    this.firebase = firebase;
+    this.user = null;
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
   }
 
+  _onAuthStateChanged(user) {
+    if (user) {
+      // get all the infos
+      this.user = user;
+    } else {
+      // user just signed out
+      this.user = null;
+    }
+  }
+
+  async signIn(email, password) {
+    const result = await firebase.auth().signInWithEmailAndPassword(email, password);
+    return true;
+  }
+
+  async signOut() {
+    const result = firebase.auth().signOut();
+    return true;
+  }
 }
