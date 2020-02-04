c3bc0374c993b780297e53684d29e8de558ae481
Stanton Parham <stanton8parham8@gmail.com>
2020-02-04 16:17:18 -0500

    Add Firebase configuration (this is considered public info)

---
+++ pwa/src/Firebase/firebaseconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyArPzEUAvPNCc2MvbWEAJZqHlP_APzAVpg",
+  authDomain: "ncsu-wolftrack.firebaseapp.com",
+  databaseURL: "https://ncsu-wolftrack.firebaseio.com",
+  projectId: "ncsu-wolftrack",
+  storageBucket: "ncsu-wolftrack.appspot.com",
+  messagingSenderId: "111084165631",
+  appId: "1:111084165631:web:e2bb2d4a59f846b00eafca"
+};
