614accc7b5c6daf579914921b0d05092a3d20177
tmleibe2 <tmleibe2@ncsu.edu>
2020-02-15 12:42:48 -0500

    started ObserverDisplay branch

--- pwa/src/ObserverDisplay/ObserverDisplay.js
+++ pwa/src/ObserverDisplay/ObserverDisplay.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { DEFAULT_DATA } from '../BLEClient/DEFAULT_DATA';
 
 function ObserverDisplay() {
+  const [data, setData] = useState(DEFAULT_DATA);
+
   return (
     <div className='ObserverDisplay'>
     </div>
