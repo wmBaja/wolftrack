5c6d5bdb8b1e9f051b930f78c83fb14274d502d9
tmleibe2 <tmleibe2@ncsu.edu>
2020-02-15 14:33:03 -0500

    finalized graph object

--- pwa/src/ObserverDisplay/Graph/Graph.js
+++ pwa/src/ObserverDisplay/Graph/Graph.js
@@ -2,30 +2,18 @@ import Dygraph from 'dygraphs';
 import React from 'react';
 
 class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = props.data;
+    this.options = props.options;
+  }
 
-
-    render() {
-      return <div ref="chart"></div>;
-    }
+  render() {
+    return <div ref="chart"></div>;
+  }
 
   componentDidMount() {
-    //const messages = [0, 2, 4, 6, 8];
-
-    var data = [
-      [1,10],
-      [2,20],
-      [3,50],
-      [4,70]
-    ] ;
-    /*messages.forEach((response) => {
-      data += response[0] + ',' + response[1] + "\n";
-    });*/
-
-    new Dygraph(this.refs.chart, data, {
-      drawPoints: true,
-      showRoller: true,
-      labels: ['X', 'A'],
-    });
+    new Dygraph(this.refs.chart, this.data, this.options);
   }
 
 }
