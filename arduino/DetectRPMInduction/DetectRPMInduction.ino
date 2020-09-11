8d325dca8357dfe7dc588d8d08e6df56e1892ee1
Stanton Parham <stanton8parham8@gmail.com>
2020-09-11 09:15:26 -0400

    Consolidate RPMInduction sketches; delete old versions

--- arduino/DetectRPMInduction/DetectRPMInduction.ino
+++ arduino/DetectRPMInduction/DetectRPMInduction.ino
@@ -1,70 +1,97 @@
-// the pin used to measure engine RPM
-#define ENGINE_RPM_INDUCTION_PIN A1
-// TODO the constants below should be changed to preprocessor defines
-// the number of milliseconds in a second
-int MILLIS_IN_SEC = 1000;
-// the number of seconds in a minute
-int SECS_IN_MIN = 60;
-
-// the amount of time between updates (in ms)
-int UPDATE_PERIOD = 1000;
-// the conversion factor for sparks per update period to sparks per minute
-int SPARKS_PER_UPDATE_TO_SPARKS_PER_MIN = SECS_IN_MIN / (UPDATE_PERIOD / MILLIS_IN_SEC);
-
-// voltage spike threshold (in terms of a range of 0-5V mapped into a 0-1023 range)
-int THRESHOLD = 900; // (1 volt)
-
-// the maximum amount of time between spikes (in ms)
-int MAX_SPIKE_INTERVAL = 5;
+// number of microseconds in a minute
+#define MICROS_IN_MIN 60000000
+// the baud rate for the serial connection
+#define BAUD_RATE 115200
+// amount of time between transmissions to RPi (in ms)
+#define TRANSMISSION_INTERVAL 1000
 
 // the current time (in ms)
 unsigned long curTime = 0;
-// the next time to print an update (in ms)
-unsigned long nextUpdateTime = 0;
-// the last time that a voltage spike occurred (in ms)
-unsigned long lastSpikeTime = 0;
-// the total number of sparks in this update period
+// the time at which the next data packet should be sent to the RPi (in ms)
+unsigned long nextTransmissionTime = 0;
+
+
+// amount of time between RPM (engine and CVT secondary) updates (in ms)
+#define RPM_UPDATE_INTERVAL 1000
+// the pin used to measure engine RPM
+#define ENGINE_RPM_INDUCTION_PIN 2
+
+// the next time to calculate the RPM (engine and secondary) (in ms)
+unsigned long nextRPMUpdateTime = 0;
+// the most recently calculated engine RPM
+int engineRPM = 0;
+
+// whether or not the engine is currently sparking
+bool sparking = false;
+// the number of sparks recorded during the current update interval
 int numSparks = 0;
+// the sum of all spark intervals (in us) recorded during current the update interval
+unsigned long sparkIntervalSum = 0;
+// the last time a spark was detected (in us)
+unsigned long lastSparkTime = 0;
 
+void checkForSparks() {
+  int pinState = digitalRead(ENGINE_RPM_INDUCTION_PIN);
 
+  // if the pin is high
+  if (pinState) {
+    // if the engine is not considered sparking
+    if (!sparking) {
+      // then this is a new spark
+      numSparks++;
+
+      // get the current time
+      unsigned long curTimeMicros = micros();
+      // add the last spark interval to the sum
+      sparkIntervalSum += curTimeMicros - lastSparkTime;
+      // update the last spark time to be this time
+      lastSparkTime = curTimeMicros;
+
+      // now we need to consider the engine as sparking
+      sparking = true;
+    }
+  } else { // else the pin is low
+    // thus the engine is not sparking
+    sparking = false;
+  }
+}
+
+void updateRPMs() {
+  // if it's time to update the RPMs
+  if (curTime > nextRPMUpdateTime) {
+    // calculate the average spark interval
+    unsigned long avgSparkInterval = sparkIntervalSum / numSparks; // could increase accuracy by using floating point ops
+
+    // calculate the engine RPM based on the average spark interval
+    engineRPM = MICROS_IN_MIN / avgSparkInterval; // could increase accuracy by using floating point ops
+
+    // reset the number of sparks and spark interval sum
+    numSparks = 0;
+    sparkIntervalSum = 0;
+
+    // calculate the next update time
+    nextRPMUpdateTime = curTime + RPM_UPDATE_INTERVAL;
+  }
+}
 
 void setup() {
-  // establish serial connection
-  Serial.begin(115200);
-  // calculate the next print time
+  Serial.begin(BAUD_RATE);
+  pinMode(ENGINE_RPM_INDUCTION_PIN, INPUT);
   curTime = millis();
-  nextUpdateTime = curTime + UPDATE_PERIOD;
+  nextTransmissionTime = curTime + TRANSMISSION_INTERVAL;
+  nextRPMUpdateTime = curTime + RPM_UPDATE_INTERVAL;
 }
 
 void loop() {
-  // get sensor reading
-  int sensorValue = analogRead(ENGINE_RPM_INDUCTION_PIN);
-  // determine if there is currently a voltage spike
-  bool isSpiking = sensorValue > THRESHOLD ? true : false;
-//  Serial.println(sensorValue);
-  // get the current time
   curTime = millis();
 
-  // if a voltage spike is detected
-  if (isSpiking) {
-    // if the last spike occurred over MAX_SPIKE_INTERVAL milliseconds ago
-    if (curTime > lastSpikeTime + MAX_SPIKE_INTERVAL) {
-      // then a new spark is starting
-      numSparks++;
-    }
-    // this time is the new last spike time
-    lastSpikeTime = curTime;
-  }
+  checkForSparks();
+  updateRPMs();
 
-  // update every update period
-  if (curTime > nextUpdateTime) {
-    // calculate the RPM from the num sparks in this update period
-    int rpm = numSparks * SPARKS_PER_UPDATE_TO_SPARKS_PER_MIN;
-    // report RPM
-    Serial.println(rpm);
-    // calculate the next update time
-    nextUpdateTime = curTime + UPDATE_PERIOD;
-    // reset num of sparks
-    numSparks = 0;
+  // if it's time to transmit
+  if (curTime > nextTransmissionTime) {
+    Serial.println(engineRPM);
+    // calculate the next transmission time
+    nextTransmissionTime = curTime + TRANSMISSION_INTERVAL;
   }
 }
