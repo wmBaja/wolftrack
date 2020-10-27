145373af20645efdde68fd46a3f5227789227de8
Stanton Parham <stanton8parham8@gmail.com>
2020-10-27 12:31:18 -0400

    Implement reading shock compression data from MCP3008

--- esp32/Wolftrack/src/sensors/ShockCompression.cpp
+++ esp32/Wolftrack/src/sensors/ShockCompression.cpp
@@ -3,16 +3,28 @@
 #include "ShockCompression.h"
 #include "../../config.h"
 
-ShockCompression::ShockCompression():
-  shockCompression(0)
+ShockCompression::ShockCompression(MCP3008* adc, uint8_t adcChannel):
+  adc(adc),
+  adcChannel(adcChannel),
+  shockCompression(0),
+  nextReadTime(0)
 {}
 
 void ShockCompression::loop() {
 #if GENERATE_RANDOM_VALUES
   this->shockCompression = random(1024);
 #else
-  // TODO need to implement
-  this->shockCompression = random(1024);
+  unsigned long curTime = millis();
+
+  if (curTime > this->nextReadTime) {
+    uint32_t sensorValue = this->adc->analogRead(this->adcChannel);
+    double actuationIn = 6 - (sensorValue / 1023.0) * 6; // range: 0 - 6
+    // TODO implement conversion from real value to integer value
+    this->shockCompression = (int) actuationIn;
+
+    // calculate the next read time
+    this->nextReadTime = curTime + READ_INTERVAL;
+  }
 #endif
 }
 
--- esp32/Wolftrack/src/sensors/ShockCompression.h
+++ esp32/Wolftrack/src/sensors/ShockCompression.h
@@ -2,14 +2,22 @@
 #define _SHOCK_COMPRESSION_H
 
 #include <stdbool.h>
+#include <MCP3XXX.h>
 
 #include "Sensor.h"
 
+// amount of time between sensor reads
+#define READ_INTERVAL 1000
+
 class ShockCompression: public Sensor {
+    MCP3008* adc;
+    uint8_t adcChannel;
     // the most recently calculated shock compression
     int shockCompression;
+    // the time at which to read a new value from the sensor
+    unsigned long nextReadTime;
   public:
-    ShockCompression();
+    ShockCompression(MCP3008*, uint8_t);
     void loop();
     int getValue();
 };
