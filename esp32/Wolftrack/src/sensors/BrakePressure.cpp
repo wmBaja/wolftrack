9055ac8591a0ff1f165eaef176acef3c55baa021
Stanton Parham <stanton8parham8@gmail.com>
2020-10-27 12:29:24 -0400

    Implement reading for brake pressure from MCP3008

--- esp32/Wolftrack/src/sensors/BrakePressure.cpp
+++ esp32/Wolftrack/src/sensors/BrakePressure.cpp
@@ -3,16 +3,26 @@
 #include "BrakePressure.h"
 #include "../../config.h"
 
-BrakePressure::BrakePressure():
-  brakePressure(0)
+BrakePressure::BrakePressure(MCP3008* adc, uint8_t adcChannel):
+  adc(adc),
+  adcChannel(adcChannel),
+  brakePressure(0),
+  nextReadTime(0)
 {}
 
 void BrakePressure::loop() {
 #if GENERATE_RANDOM_VALUES
   this->brakePressure = random(2000);
 #else
-  // TODO need to implement
-  this->brakePressure = random(2000);
+  unsigned long curTime = millis();
+
+  if (curTime > this->nextReadTime) {
+    uint32_t analogValue = this->adc->analogRead(this->adcChannel);
+    this->brakePressure = (analogValue / 1023.0) * 2000; // range: 0 - 2000
+
+    // calculate the next read time
+    this->nextReadTime = curTime + READ_INTERVAL;
+  }
 #endif
 }
 
